50c13e8175e078dbd23f685dcfda476bfba11d9c
David Konsumer <konsumer@jetboystudio.com>
2017-11-07 20:00:14 -0800

    standard

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
-const path = require('path');
+const path = require('path')
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
+  const { createPage } = boundActionCreators
 
   return graphql(`
     {
@@ -23,14 +23,14 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     }
   `).then(result => {
     if (result.errors) {
-      return Promise.reject(result.errors);
+      return Promise.reject(result.errors)
     }
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.path,
         component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
         context: {} // additional data can be passed via context
-      });
-    });
-  });
-};
+      })
+    })
+  })
+}
