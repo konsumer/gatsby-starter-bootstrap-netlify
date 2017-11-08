1d2889762be111cb05af690f227a3a44e9399a47
David Konsumer <konsumer@jetboystudio.com>
2017-11-07 20:31:58 -0800

    gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,17 @@ module.exports = {
           }
         ]
       }
+    },
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        headers: {},                                  // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [],                           // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true,                   // boolean to turn off the default security headers
+        mergeLinkHeaders: false,                      // boolean to turn off the default gatsby js headers (disabled by default, until gzip is fixed for server push)
+        mergeCachingHeaders: true,                    // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers // optional transform for manipulating headers under each path (e.g.sorting), etc.
+      }
     }
   ]
 }
