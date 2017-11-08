c6dd3be856a96aba0e9c889b603ae9dd706dc13f
David Konsumer <konsumer@jetboystudio.com>
2017-11-07 22:50:36 -0800

    still working on images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter'
+    title: 'Gatsby Starter Blog',
+    author: 'Kyle Mathews'
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -16,34 +15,28 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          'gatsby-remark-responsive-image',
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-            options: {
-              imagesPath: '*',
-              linksPath: '*'
-            }
-          },
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 800,
-              linkImagesToOriginal: false
+              maxWidth: 590
             }
-          }
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem'
+            }
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants'
         ]
       }
     },
-    {
-      resolve: 'gatsby-plugin-netlify',
-      options: {
-        headers: {},                                  // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [],                           // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true,                   // boolean to turn off the default security headers
-        mergeLinkHeaders: false,                      // boolean to turn off the default gatsby js headers (disabled by default, until gzip is fixed for server push)
-        mergeCachingHeaders: true,                    // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers // optional transform for manipulating headers under each path (e.g.sorting), etc.
-      }
-    }
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
   ]
 }
--- src/pages/blog/2017-11-07-this-was-made-in-the-admin-complete-with-an-image.md
+++ src/pages/blog/2017-11-07-this-was-made-in-the-admin-complete-with-an-image.md
@@ -1,9 +1,9 @@
 ---
 templateKey: blog-post
-path: madeinadmin
+path: /madeinadmin
 title: 'This was made in the admin, complete with an image'
 date: 2017-11-08T06:15:05.907Z
 ---
-![skull image](/img/tumblr_npa7ksI5vy1ssby0io1_540.gif)
+![skull image](./img/tumblr_npa7ksI5vy1ssby0io1_540.gif)
 
 It's alive!
