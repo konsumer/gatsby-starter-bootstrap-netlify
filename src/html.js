4b2b6defb070d0ae51c99cac8e78417fef69a6ec
David Konsumer <konsumer@jetboystudio.com>
2018-11-20 02:55:22 -0800

    get builds working

--- src/html.js
+++ src/html.js
@@ -1,25 +1,7 @@
 import React from 'react'
 
-let stylesStr
-if (process.env.NODE_ENV === `production`) {
-  try {
-    stylesStr = require(`!raw-loader!../public/styles.css`)
-  } catch (e) {
-    console.log(e)
-  }
-}
-
 export default class HTML extends React.Component {
   render () {
-    let css
-    if (process.env.NODE_ENV === `production`) {
-      css = (
-        <style
-          id='gatsby-inlined-css'
-          dangerouslySetInnerHTML={{ __html: stylesStr }}
-        />
-      )
-    }
     return (
       <html {...this.props.htmlAttributes}>
         <head>
@@ -32,7 +14,6 @@ export default class HTML extends React.Component {
           <script src='https://identity.netlify.com/v1/netlify-identity-widget.js' />
           <script src='/admin/netlify_redirect.js' />
           {this.props.headComponents}
-          {css}
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
